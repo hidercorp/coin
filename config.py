7458fb5a42c4191af4bd67ce7dff5157ab044d60
hidercorp <niko.erynts@gmail.com>
2018-07-15 00:41:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 # webhook
-URL = os.environ.get('URL')
+URL = os.environ.get('https://app-twxapi.herokuapp.com/')
 PORT = int(os.environ.get('PORT', '5000'))
 
 # telegram
-TOKEN = os.environ.get('TOKEN')
+TOKEN = os.environ.get('524768301:AAFk8f1gyqdkU8lVgfBoeaXj-mTFh7LRHos')
